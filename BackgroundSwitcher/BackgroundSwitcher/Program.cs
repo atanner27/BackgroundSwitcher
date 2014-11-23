8d9a7d93f1df35476ca79f1b868e1c9736081378
atanner27 <atanner2727@gmail.com>
2014-11-23 13:16:26 -0500

    Basic Setup
    Found some code to set wallpapers, needs to be rewritten and commented.
    Basic setup is done

---
+++ BackgroundSwitcher/BackgroundSwitcher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackgroundSwitcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wallpaper Test = new Wallpaper();
+            Wallpaper.Style testing = (Wallpaper.Style)2;
+            Uri location = new Uri("");
+            //location.
+        }
+    }
+}
+
+
