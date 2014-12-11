51297aa64dc585fe1cd87217c8cb811f8dacbaae
Andrew Tanner <atanner2727@gmail.com>
2014-12-11 14:06:56 -0500

    Adding Xcode Project

---
+++ BackgroundChangerMac/BackgroundChangerMac/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BackgroundChangerMac
+//
+//  Created by AndrewT on 2014-12-11.
+//  Copyright (c) 2014 AndrewT. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ BackgroundChangerMac/BackgroundChangerMac/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  BackgroundChangerMac
+//
+//  Created by AndrewT on 2014-12-11.
+//  Copyright (c) 2014 AndrewT. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
